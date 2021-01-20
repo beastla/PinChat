6e9689a32b958da80cf6c5cf637cce1cd88c0b01
beastla <77716963+beastla@users.noreply.github.com>
2021-01-20 12:27:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,8 @@ session_destroy();
     </div>
 </div>
 </div>
+  
+  
 
 <!-- Modal -->
 <div class=" modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
